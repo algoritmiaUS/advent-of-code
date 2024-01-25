2dfcd9092ccdd81b455a506ce986a691a85e5606
Kenny Flores <kennyjesus@protonmail.com>
2024-01-25 21:41:04 +0100

    Add Julia Files

---
+++ 2023/day5/julia/day5.jl
@@ -0,0 +1,15 @@
+function preprocessing(data)
+
+end
+
+function day5()
+    f = open("day5/input.txt", "r")
+    lines = readlines(f)
+    close(f)
+
+    # data = preprocessing(lines)
+
+end
+
+
+day5()
